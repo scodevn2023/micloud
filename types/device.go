4bf439429b1a680a8996d91d769b65fd912b5e4b
scodevn2023 <134914694+scodevn2023@users.noreply.github.com>
2024-05-28 10:45:16 +0700

    Update device.go

--- types/device.go
+++ types/device.go
@@ -1,44 +1,44 @@
 package types
 
 type (
-	DeviceNetWork struct {
-		IP      string `json:"localIp"`
-		Mask    string `json:"mask"`
-		Gateway string `json:"gw"`
-	}
+    DeviceNetWork struct {
+        IP      string `json:"localIp"`
+        Mask    string `json:"mask"`
+        Gateway string `json:"gw"`
+    }
 
-	DeviceInfo struct {
-		UID         int            `json:"uid"`
-		Mac         string         `json:"mac"`
-		Life        int            `json:"life"`
-		Model       string         `json:"model"`
-		Token       string         `json:"token"`
-		IpFlag      int            `json:"ipflag"`
-		Version     string         `json:"miio_ver"`
-		FireVer     string         `json:"fw_ver"`
-		HardVersion string         `json:"hw_ver"`
-		Network     *DeviceNetWork `json:"netif"`
-	}
+    DeviceInfo struct {
+        UID         int            `json:"uid"`
+        Mac         string         `json:"mac"`
+        Life        int            `json:"life"`
+        Model       string         `json:"model"`
+        Token       string         `json:"token"`
+        IpFlag      int            `json:"ipflag"`
+        Version     string         `json:"miio_ver"`
+        FireVer     string         `json:"fw_ver"`
+        HardVersion string         `json:"hw_ver"`
+        Network     *DeviceNetWork `json:"netif"`
+    }
 
-	DeviceAttribute struct {
-		Attribute string `json:"attr"`
-		Value     any    `json:"value"`
-	}
+    DeviceAttribute struct {
+        Attribute string      `json:"attr"`
+        Value     interface{} `json:"value"`
+    }
 
-	DeviceProperty struct {
-		DID     string `json:"did"`
-		SIID    int    `json:"siid"`
-		PIID    int    `json:"piid"`
-		Value   any    `json:"value,omitempty"`
-		Code    int    `json:"code,omitempty"`
-		Modtime int64  `json:"updateTime,omitempty"`
-	}
+    DeviceProperty struct {
+        DID     string      `json:"did"`
+        SIID    int         `json:"siid"`
+        PIID    int         `json:"piid"`
+        Value   interface{} `json:"value,omitempty"`
+        Code    int         `json:"code,omitempty"`
+        Modtime int64       `json:"updateTime,omitempty"`
+    }
 
-	DeviceAction struct {
-		DID  string `json:"did"`
-		SIID int    `json:"siid"`
-		AIID int    `json:"aiid"`
-		In   []any  `json:"in"`
-		Out  []any  `json:"out,omitempty"`
-	}
+    DeviceAction struct {
+        DID  string        `json:"did"`
+        SIID int           `json:"siid"`
+        AIID int           `json:"aiid"`
+        In   []interface{} `json:"in"`
+        Out  []interface{} `json:"out,omitempty"`
+    }
 )
