222633946334a87f7454c3bceec8127d2f38300a
scodevn2023 <134914694+scodevn2023@users.noreply.github.com>
2024-05-28 11:46:13 +0700

    update

---
+++ types.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+const (
+	//DefaultPort 默认的端口号
+	DefaultPort = 54321
+
+	HeadLength = 32
+)
+
+var (
+	ErrorTokenInvalid = errors.New("token invalid")
+)
+
+type (
+	CommandRequest struct {
+		ID     int    `json:"id"`
+		Method string `json:"method"`
+		Params any    `json:"params"`
+	}
+
+	CommandError struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+
+	CommandResponse struct {
+		ID          int             `json:"id"`
+		Method      string          `json:"method"`
+		Result      json.RawMessage `json:"result"`
+		Error       *CommandError   `json:"error"`
+		ExecuteTime int             `json:"exe_time"`
+	}
+)
+
+func (e *CommandError) Error() string {
+	return e.Message
+}
+
+func NewCommandRequest(method string, params any) *CommandRequest {
+	return &CommandRequest{
+		Method: method,
+		Params: params,
+	}
+}
+
+func (req *CommandRequest) Bytes() ([]byte, error) {
+	return json.Marshal(req)
+}
+
+func (res *CommandResponse) Decode(v any) (err error) {
+	return json.Unmarshal(res.Result, v)
+}
